43def2cd6e8fde8e56e691c9009b1cd3f4b3545f
anischali <anis.chali1@outlook.com>
2018-11-05 00:34:14 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include "negamax.h"
+
+void print_usage(void){
+    cout << "Usage of negmax to tic tac toe\nNEGAMAX <state> <player>" << endl;
+    cout << "Example: NEGAMAX .X.O..... ONE" << endl;
+    cout << "ONE -- player X" << endl;
+    cout << "TWO -- player O" << endl;
+    exit(-1);
+}
+
+ivec_t parse_state(char const *argv){
+    ivec_t s;
+    int c = 0;
+    int p = 0;
+    if ((int)strlen(argv) != BAORD_SIZE)
+        print_usage();
+    for (c = 0; c < BAORD_SIZE; ++c){
+        p = ((argv[c] == '.') ? 0 : ((argv[c] != 'X') ? -1 : 1));
+        s.push_back(p);
+        
+    }
+    return s;
+}
+
+
+int parse_player(char const *argv){
+    if (strncmp(argv,"ONE",3) == 0)
+        return 1;
+    else if (strncmp(argv,"TWO",3) == 0)
+    {
+        return -1;
+    }
+    print_usage(); 
+    return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    if (argc < 2){
+        print_usage();
+    }
+    srand(time(NULL));
+    board_t b = {};
+    b.state = parse_state(argv[1]);
+    b.next_player = parse_player(argv[2]);
+    b.winner = 0;
+    b.full = false;
+    board_t result = negamax(b,0,PLAYER_ONE);
+    printf("The best move is: %d\n",result.move);
+
+    return 0;
+}
+
+
